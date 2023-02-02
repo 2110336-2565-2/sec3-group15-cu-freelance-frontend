598c279d9dec0e5a2712c08c0116078c34cccfea
nartgg35665 <77729065+nartgg35665@users.noreply.github.com>
2023-02-02 14:26:51 +0700

    fix setting of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import "twin.macro"
+import { Routes,Route } from 'react-router-dom'
+
+import HomePage from './pages/Home'
+import LoginPage from './pages/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div tw='text-6xl'> TEST </div>
-    </div>
-  )
+  return <Routes>
+    <Route path="/home" element={<HomePage/>}/>
+    <Route path="/login" element={<LoginPage/>}/>
+  </Routes>
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import GlobalStyles from './styles/GlobalStyles'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GlobalStyles from "./styles/GlobalStyles";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <BrowserRouter>
+      {" "}
       <GlobalStyles />
       <App />
-  </React.StrictMode>,
-)
+    </BrowserRouter>
+  </React.StrictMode>
+);
