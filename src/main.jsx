acdf04f966eabfad05209ad6b02b4232ebc65c7b
patcharapongk <6230362521@student.chula.ac.th>
2023-02-03 01:04:57 +0700

    Added Poppins font

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import GlobalStyles from './styles/GlobalStyles'
 import './index.css'
+
+// twin macro css settings
+import GlobalStyles from './styles/GlobalStyles'
+import './styles/Fonts.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
