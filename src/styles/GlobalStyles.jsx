f50eb0eff4f7e5146e7b15d5baddfab7a8e54461
patcharapongk <6230362521@student.chula.ac.th>
2023-02-02 23:29:38 +0700

    --- RE-SETUP (2nd), Vite + React + Twin.macro ----

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Global } from '@emotion/react'
-import tw, { css, theme, GlobalStyles as BaseStyles } from 'twin.macro'
+import { Global, css } from '@emotion/react'
+import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro'
 
-const customStyles = css({
-  body: {
-    WebkitTapHighlightColor: theme`colors.purple.500`,
-    ...tw`antialiased`,
-  },
-})
+const customStyles = css`
+  @font-face {
+    font-family: 'Foo';
+    src: url('/path/to/exampleFont.woff') format('woff');
+    font-style: normal;
+    font-weight: 400;
+    /* https://styled-components.com/docs/faqs#how-do-i-fix-flickering-text-after-server-side-rendering */
+    font-display: fallback;
+  }
+`
 
 const GlobalStyles = () => (
   <>
