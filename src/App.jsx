a25bdfcbfed68fb67bae2ebf964c222bb00ec1bd
nartgg35665 <77729065+nartgg35665@users.noreply.github.com>
2023-02-03 14:55:55 +0700

    set routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import tw from 'twin.macro'
+import { Routes,Route,Navigate } from 'react-router-dom'
+import HomePage from './pages/Home'
+import LoginPage from './pages/Login'
+import RegisterPage from './pages/Register'
+import ProfilePage from './pages/Profile'
 
 function App() {
 
   return (
-    <div className="App">
-
-      <h1 tw="text-6xl">React App</h1>
-    </div>
+    <Routes>
+      <Route path="/home" element={<HomePage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/register" element={<RegisterPage />}/>
+      <Route path="/profile" element={<ProfilePage/>}/>
+      <Route path="*" element={<Navigate to="/home"/>}/>
+    </Routes>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import './index.css'
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
 
 // twin macro css settings
-import GlobalStyles from './styles/GlobalStyles'
-import './styles/Fonts.css'
+import GlobalStyles from "./styles/GlobalStyles";
+import "./styles/Fonts.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <GlobalStyles />
-    <App />
-  </React.StrictMode>,
-)
+    <GlobalStyles />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
