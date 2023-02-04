efe5057dcf2b5278c16aa248d0ab8c39968ae3ca
nartgg35665 <77729065+nartgg35665@users.noreply.github.com>
2023-02-05 01:27:05 +0700

    updated font size,font family

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,6 +1,6 @@
 import tw from "twin.macro";
 import HumanImg from "../../assets/human.png";
-
+import searchIcon from "../../assets/searchIcon.svg";
 const Wrapper = tw.div`
 w-[90%] max-w-[1200px] mx-auto
 h-[70vh]
@@ -14,29 +14,29 @@ const LeftWrapper = tw.div`
 flex flex-col
 gap-8
 justify-center
-w-[60%]
+w-[50%]
 `;
 const TextWrapper = tw.div`
     text-white
     flex flex-col
     gap-4
 `;
-const TextHeader1 = tw.h1`text-5xl font-semibold
+const TextHeader1 = tw.h1`tracking-wide text-7xl font-semibold
 font-ibm
 `;
-const TextHeader2 = tw.h1`text-6xl font-bold
+const TextHeader2 = tw.h1`text-8xl font-bold
 font-ibm
 drop-shadow-md
 `;
-const TextBody = tw.p`w-[49%] text-xl font-medium font-ibm drop-shadow-md`;
+const TextBody = tw.p`w-[49%] text-lg font-medium font-ibm drop-shadow-md`;
 const HighLightTextBody = tw.span`text-3xl font-medium font-ibm drop-shadow-md`;
-const InputWrapper = tw.input`
-    w-[80%]
-    px-2 py-2 
-    rounded-xl
-`;
+// const InputWrapper = tw.input`
+//     w-[80%]
+//     px-2 py-2 
+//     rounded-xl
+// `;
 
-const RightWrapper = tw.div`w-[40%] flex justify-center`;
+const RightWrapper = tw.div`w-[50%] flex justify-center`;
 const ImageWrapper = tw.img`object-contain`;
 
 const Hero = () => {
@@ -52,7 +52,20 @@ const Hero = () => {
             <HighLightTextBody> จริง</HighLightTextBody>
           </TextBody>
         </TextWrapper>
-        <InputWrapper type="text" placeholder="Search for a home" />
+        <div
+          tw="relative  w-[50%] h-[6%] rounded-md bg-white min-w-[220px]  text-[#D62B70]"  
+        >
+          <input
+            type="text"
+            placeholder="กำลังตามหาอะไรอยู่..."
+            tw="border-none placeholder:text-[#F4B8DA] focus:outline-none bg-transparent ml-[2%] mr-[15%] w-[80%] h-[100%] text-base font-ibm"
+          />
+          <img
+            src={searchIcon}
+            alt="searchIcon"
+            tw="w-[9%] absolute left-[90%] top-[50%] translate-y-[-50%] "
+          />
+        </div>
       </LeftWrapper>
       <RightWrapper>
         <ImageWrapper src={HumanImg} />
