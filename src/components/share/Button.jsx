22b92c8b5d239b64ca46f928554787439f5c3f9f
nartgg35665 <77729065+nartgg35665@users.noreply.github.com>
2023-02-04 13:15:45 +0700

    modify button

--- src/components/share/Button.jsx
+++ src/components/share/Button.jsx
@@ -7,7 +7,7 @@ const Button = (props) => {
     props.onClick();
   };
   const ButtonNoBG = tw.button`text-[#D62B70]  text-[10px] text-center`;
-  const ButtonBG = tw.button`text-white bg-[#D62B70] rounded-[4px] text-[10px] p-[0.1rem]`;
+  const ButtonBG = tw.button`text-white bg-[#D62B70] rounded-[4px] text-[10px] p-[0.3em]`;
 
   let Button;
   if (noBG) Button = ButtonNoBG;
