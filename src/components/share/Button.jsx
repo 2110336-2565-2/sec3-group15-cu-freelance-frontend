30ffd8c289392e34c5e34582b9a1d1bdb13220e1
nartgg35665 <77729065+nartgg35665@users.noreply.github.com>
2023-02-04 13:11:11 +0700

    create button

--- src/components/share/Button.jsx
+++ src/components/share/Button.jsx
@@ -6,8 +6,8 @@ const Button = (props) => {
   const onClickHandler = () => {
     props.onClick();
   };
-  const ButtonNoBG = tw.button`text-[#D62B70]  text-[20px]`;
-  const ButtonBG = tw.button`text-white bg-[#D62B70] rounded-[4px] text-[36px]`;
+  const ButtonNoBG = tw.button`text-[#D62B70]  text-[10px] text-center`;
+  const ButtonBG = tw.button`text-white bg-[#D62B70] rounded-[4px] text-[10px] p-[0.1rem]`;
 
   let Button;
   if (noBG) Button = ButtonNoBG;
