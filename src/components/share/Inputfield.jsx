fc3dfe6d8c49cf640f9afb5084d2100f5766f185
Theerut  Seekeaw <theerut@MacBook-Air-khxng-Theerut.local>
2023-02-06 22:03:57 +0700

    Updated Login Page

--- src/components/share/Inputfield.jsx
+++ src/components/share/Inputfield.jsx
@@ -1,20 +1,86 @@
-import tw from "twin.macro"
-import Card from "../share/Card"
-const styles = {
-   container:()=>[
-    tw`flex flex-col font-inter items-center`
-   ],
-   header:()=>[
-    tw`text-[48px] font-bold text-[#D62B70]`
-   ],
-   form:()=>[
-    // tw`text-[#E68BBE] flex flex-col [> input]:(border-black)`
-   ]
-}
-const InputField= ()=>{
-    const Input = tw.input`border-[#E68BBE] border-[5px] rounded-[10px]`
-    return (
-        <div></div>
-    )
-}
-export default LoginForm;
+import React, { useReducer, useEffect } from 'react';
+
+import { validate } from '../../util/validators';
+import './Input.css';
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators)
+      };
+    case 'TOUCH': {
+      return {
+        ...state,
+        isTouched: true
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+const Input = props => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: props.initialValue || '',
+    isTouched: false,
+    isValid: props.initialValid || false
+  });
+
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid, onInput]);
+
+  const changeHandler = event => {
+    dispatch({
+      type: 'CHANGE',
+      val: event.target.value,
+      validators: props.validators
+    });
+  };
+
+  const touchHandler = () => {
+    dispatch({
+      type: 'TOUCH'
+    });
+  };
+
+  const element =
+    props.element === 'input' ? (
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    ) : (
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    );
+
+  return (
+    <div
+      className={`form-control ${!inputState.isValid &&
+        inputState.isTouched &&
+        'form-control--invalid'}`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      {element}
+      {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
+    </div>
+  );
+};
+
+export default Input;
