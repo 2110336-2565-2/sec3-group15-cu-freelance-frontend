6fdf2be5ee4f086ae51cedd6ad753bdf88bfd98e
nartgg35665 <77729065+nartgg35665@users.noreply.github.com>
2023-02-06 20:20:47 +0700

    Fixed footer

--- src/components/share/Footer.jsx
+++ src/components/share/Footer.jsx
@@ -1,16 +1,12 @@
 import tw from "twin.macro";
 
 const Footer = () => {
-  const Wrapper = tw.div`w-[100vw]`;
-  const HeaderWrapper = tw.div`w-[90%] max-w-[1150px] mx-auto h-[5vh] text-sm font-sans font-normal text-[#D62B70] flex justify-between`;
+  const HeaderWrapper = tw.div`w-[100vw] h-[5vh] max-w-[1150px] mx-auto text-sm font-sans font-normal text-[#D62B70] flex justify-between`;
   return (
-    <Wrapper>
-      {" "}
       <HeaderWrapper>
         <div>© CU Freelance 2023</div>
         <div>Contact | Privacy Policy | Terms and Conditions</div>
       </HeaderWrapper>
-    </Wrapper>
   );
 };
 
