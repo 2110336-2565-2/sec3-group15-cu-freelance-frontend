cbc26a1b2ba92f882b232228ce9e6d9620742c88
nartgg35665 <77729065+nartgg35665@users.noreply.github.com>
2023-02-06 17:23:37 +0700

    Added card-wrapper to home/category

--- src/components/home/category/Index.jsx
+++ src/components/home/category/Index.jsx
@@ -1,11 +1,15 @@
 import tw from "twin.macro";
 import Button from "../../share/Button";
 const CategoryIndex = () => {
+
+  const CardWrapper=tw.div`h-[80%] w-[100%] flex`;
+
   return (
     <div tw="h-[100vh] w-[90%] max-w-[1200px] mx-auto bg-white pt-[15vh]">
       <div tw="pl-[2%] font-ibm font-bold text-[#D62B70] text-3xl flex justify-between">
         เลือกฟรีเเลนซ์จากหมวดหมู่เหล่านี้ <Button>ดูฟรีแลนซ์ทั้งหมด</Button>
       </div>
+      <CardWrapper></CardWrapper>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,11 @@
 import tw from "twin.macro";
-import PortfolioCard from "../components/share/PortfolioCard";
-import ProfilePic from "../assets/ProfilePic.png"
-import PortfolioImg from "../assets/PortfolioImg.png"
-import CategoryCard from "../components/share/CategoryCard";
-import TrendingIcon from "../assets/CheckIcon.png"
-import CommentCard from "../components/share/CommentCard";
+import CategoryIndex from "../components/Home/category/Index";
+import Hero from "../components/Home/Hero";
 const HomePage = () => {
   return (
     <div>
-      {/* <CategoryCard imgSrc={TrendingIcon} title='Trending'/> */}
-      {/* <PortfolioCard userImgSrc={ProfilePic} portImgSrc={PortfolioImg} name="Username123" description="ออกแบบเว็บไซต์ Web Design งานคุณภาพราคาโดนๆ"/> */}
-      <CommentCard comment="ใช้ CU Freelance ช่วยประหยัด เวลาได้มากเพราะมีตัวเลือก หลากหลายทั้งผลงานและรีวิว การันตีมั่นใจได้ว่าจะได้ผลงานตรงกับ ความต้องการของเรา จริงๆ"
-                   imgSrc={ProfilePic}
-                   name="เสือใหญ่ ใจดี"
-                   position="Ceo of BooxBix"/>
+      <Hero/>
+      <CategoryIndex/>
     </div>
   );
 };
