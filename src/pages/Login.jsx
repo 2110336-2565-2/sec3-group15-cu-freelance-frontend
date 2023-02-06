c2d7a25badc26764b774f4ed48bfec95b946fdf6
Theerut  Seekeaw <theerut@MacBook-Air-khxng-Theerut.local>
2023-02-06 19:52:18 +0700

    Add login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,22 @@
+import Logo from "../assets/logo.svg"
+import tw from "twin.macro"
+import LoginForm from "../components/login/LoginForm"
+const styles={
+    container:()=>[
+        tw`flex flex-col items-center mt-[2%]`
+    ],
+    image:()=>[
+        // tw`pt-[1%]`
+    ]
+}
 const LoginPage=()=>{
 
-    return <div>Login</div>
+    return (
+        <div css={styles.container}>
+            <img css={styles.image} src={Logo}/>
+            <LoginForm/>
+        </div>
+    )
 }
 
 export default LoginPage
