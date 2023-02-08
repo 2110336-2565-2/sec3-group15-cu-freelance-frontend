94c89b7d8211889561768c07623ba0bd6bcad11a
Theerut  Seekeaw <theerut@MacBook-Air-khxng-Theerut.local>
2023-02-08 18:45:30 +0700

    Added Auth Component

---
+++ src/components/share/AuthTypeCard.jsx
@@ -0,0 +1,29 @@
+import tw from "twin.macro"
+import Card from "./Card"
+const avatarVariants={
+    left: tw`h-[100%] box-border absolute -bottom-[17%] right-[50%] translate-x-[50%]`,
+    right: tw`w-[85%] h-[100%] box-border absolute -bottom-[18%] right-[50%] translate-x-[50%] rotate-[356deg]`
+};
+const styles={
+    container:()=>[
+        tw`flex flex-col relative h-[100%]`
+    ],
+    text:()=>[
+        tw`text-[36px] text-[#D62B70] font-bold font-ibm`
+    ],
+    avatar:({type})=>[
+        avatarVariants[type]
+    ]
+};
+const AuthTypeCard=({text, avatar, type, onClick})=>{
+    return (
+        <Card type="loginType" onClick={onClick}>
+            <div css={styles.container}>
+                <div css={styles.text}>{text}</div>
+                <img css={styles.avatar({type})} src={avatar}/>
+            </div>
+        </Card>
+    )
+}
+
+export default AuthTypeCard;
