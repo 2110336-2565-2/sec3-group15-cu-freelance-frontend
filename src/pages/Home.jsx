c18cd326ba2ea9470a7e04849d337bcf07209cce
samithiwat <samithiwat2544@gmail.com>
2023-02-13 02:02:17 +0700

    fix: invalid import path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import tw from "twin.macro";
-import Hero from "../components/Home/Hero";
-import CategoryIndex from "../components/Home/category/Index";
-import CommentIndex from "../components/Home/comment/Index";
-import ValidateIndex from "../components/Home/validate/Index";
+import Hero from "../components/home/Hero";
+import CategoryIndex from "../components/home/category/Index";
+import CommentIndex from "../components/home/comment/Index";
+import ValidateIndex from "../components/home/validate/Index";
 import NavBar from "../components/share/Navbar";
 const HomePage = () => {
-  
   return (
     <div>
       <Hero />
