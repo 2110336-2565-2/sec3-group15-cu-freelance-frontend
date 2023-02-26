817e97a1e621fd1e06da6e9bd1d7c8ead6183fc0
patcharapongk <6230362521@student.chula.ac.th>
2023-02-26 20:05:52 +0700

    Added color palette to tailwind config, usage: freelance-<color name>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,8 +12,17 @@ module.exports = {
         'buttonHover' :'0px 2px 10px rgba(0, 0, 0, 0.5)',
         'navbar': '0px 2px 20px rgba(0, 0, 0, 0.25)',
         'dropnav':'0 20px 25px -5px rgb(0 0 0 / 0.25), 0 8px 10px -6px rgb(0 0 0 / 0.25);',
-      }
+      },
+      colors:{
+        'freelance': {
+          'pink': '#D62B70',
+          'black-primary' : '#151515',
+          'black-secondary': '#707070',
+          'white': '#000000'
+        }
+      },
     },
+
     screens: {
       dt: "600px",
       pf: "1400px",
