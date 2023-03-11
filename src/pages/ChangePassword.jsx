d3bdc265e5eca04f26ca2008b31d36a1ed96641b
Theerut <theerut.seek@gmail.com>
2023-03-11 14:38:50 +0700

    Added Change Password Page

--- src/pages/ChangePassword.jsx
+++ src/pages/ChangePassword.jsx
@@ -3,6 +3,10 @@ import Input from "../components/share/Input";
 import lockIcon from "../assets/LockIcon.svg"
 import { VALIDATOR_MINLENGTH, VALIDATOR_MATCH } from "../components/share/Validate";
 import { useForm } from "../hooks/form-hook";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthProvider";
+import { useContext } from "react";
+import { apiClient } from "../utils/axios";
 const Container = tw.div`flex justify-center pt-[20vh] min-h-[95vh] mb-4`;
 const Form = tw.form`flex flex-col shadow-[0_4px_4px_rgba(0,0,0,0.25)] px-8 py-4 rounded-[20px] gap-y-2 w-[420px] h-fit`;
 const Title = tw.div`text-center font-bold text-xl dt:text-3xl`;
@@ -11,6 +15,8 @@ const Caution = tw.div`text-center font-bold text-xs dt:text-sm font-inter my-4`
 const SubmitButton = tw.button`bg-[#D62B70] text-center m-2 text-white font-inter font-bold rounded-[10px] p-2`;
 const CancelButton = tw.button`text-[#D62B70] font-medium font-inter p-2`;
 const ChangePasswordPage = ()=>{
+    const navigate = useNavigate();
+    const authCtx = useContext(AuthContext);
     const [formState, inputHandler, setFormData] = useForm({
         Current: {
             value:"",
@@ -30,10 +36,19 @@ const ChangePasswordPage = ()=>{
         event.preventDefault();
         const { Current, New, Confirm} =
                 formState.inputs;
-        console.log("button click!")
+        //console.log("button click!")
         try{
-            console.log("button click!")
-            console.log(Current.value, New, Confirm)
+            let data = JSON.stringify({
+                current_password: Current.value,
+                new_password: New.value,
+                username: authCtx.userInfo.username
+              });
+              console.log(data);
+              const response = await apiClient.post(`/auth/ChangePassword`, data, {
+                headers: { "Content-Type": "application/json" },
+            });
+            //console.log(response.data);
+            navigate('/home');
         }
         catch(err){
             console.log(err);
