256145736443793c4a3de253aa7034b4a2c0e72b
nartgg35665 <77729065+nartgg35665@users.noreply.github.com>
2023-03-13 15:14:33 +0700

    Updated profile card

--- src/components/share/ProfileCard2.jsx
+++ src/components/share/ProfileCard2.jsx
@@ -1,33 +1,35 @@
 import tw from "twin.macro";
 import Card from "../share/Card";
 import Button from "./Button";
-const ProfileCard2 = ({userImage, username, portCount, avgTime})=>{
-    const Container = tw.div`flex flex-row justify-between font-ibm text-[#D62B70] w-fit gap-x-8`;
-    const LeftSection = tw.div`flex flex-col items-center text-center font-bold gap-2`;
-    const Image = tw.img``;
-    const Name = tw.div``;
-    const RightSection = tw.div`flex flex-col justify-between`;
-    const Status = tw.div`flex flex-row justify-between gap-x-8 font-semibold`
-    return (
-        <Card hasShadow={true} type="portfolio2">
-            <Container>
-                <LeftSection>
-                    <Image src={userImage}/>
-                    <Name>{username}</Name>
-                </LeftSection>
-                <RightSection>
-                    <Status>
-                        <div>{"ขายงานแล้ว"}</div>
-                        <div>{`${portCount} ครั้ง`}</div>
-                    </Status>
-                    <Status>
-                        <div>{"เวลาตอบกลับเฉลี่ย"}</div>
-                        <div>{`${avgTime} ครั้ง`}</div>
-                    </Status>
-                    <Button primary width="90%" disable={true} onClick={()=>{console.log("test")}}>แชทกับฟรีแลนซ์</Button>
-                </RightSection>
-            </Container>
-        </Card>
-    )
-}
+const ProfileCard2 = ({ userImage, username, portCount, avgTime,onClick }) => {
+  const Container = tw.div`flex flex-row justify-between font-ibm text-[#D62B70] w-fit gap-x-8`;
+  const LeftSection = tw.div`flex flex-col items-center text-center font-bold gap-2`;
+  const Image = tw.img``;
+  const Name = tw.div``;
+  const RightSection = tw.div`flex flex-col justify-between`;
+  const Status = tw.div`flex flex-row justify-between gap-x-8 font-semibold`;
+  return (
+    <Card hasShadow={true} type="portfolio2">
+      <Container>
+        <LeftSection>
+          <Image src={userImage} />
+          <Name>{username}</Name>
+        </LeftSection>
+        <RightSection>
+          <Status>
+            <div>{"ขายงานแล้ว"}</div>
+            <div>{`${portCount} ครั้ง`}</div>
+          </Status>
+          <Status>
+            <div>{"เวลาตอบกลับเฉลี่ย"}</div>
+            <div>{`${avgTime} ครั้ง`}</div>
+          </Status>
+          <Button primary width="100%" onClick={onClick}>
+            ส่งออเดอร์
+          </Button>
+        </RightSection>
+      </Container>
+    </Card>
+  );
+};
 export default ProfileCard2;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,21 +4,23 @@ import DurationIcon from "../assets/DurationIcon.svg";
 import ProfileCard from "../components/share/ProfileCard2";
 import UserImage from "../assets/profile1.svg";
 import React, { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { authClient } from "../utils/auth";
 import { apiClient } from "../utils/axios";
+import { onClickSend } from "../utils/order";
 
 const PortfolioPage = () => {
   const Container = tw.div`flex flex-col text-left my-[15vh] mx-auto px-20 text-[#D62B70] font-ibm gap-y-12 max-w-[1200px]`;
-  const FirstRow=tw.div`flex w-full justify-between`
+  const FirstRow = tw.div`flex w-full justify-between`;
   const Path = tw.div`font-ibm font-bold text-sm`;
   const Image = tw.img`self-center rounded-[30px] border-[#D62B70] border-8`;
   const Title = tw.h1`font-bold text-3xl`;
   const Description = tw.p`font-bold text-lg whitespace-pre-wrap`;
-  const Duration=tw.div`flex items-center font-bold text-2xl`
-  const ImageDuration=tw.img`mr-2`
+  const Duration = tw.div`flex items-center font-bold text-2xl`;
+  const ImageDuration = tw.img`mr-2`;
 
   const params = useParams();
+  const navigate = useNavigate();
   const location = useLocation();
   const id = params.portId;
   const [portfolio, setPortfolio] = useState(null);
@@ -29,10 +31,9 @@ const PortfolioPage = () => {
     const fetchData = async () => {
       try {
         let response;
-        if (location.pathname.slice(0, 13) === "/my-portfolio"){
+        if (location.pathname.slice(0, 13) === "/my-portfolio") {
           response = await apiClient.get(`/portfolio/me/${id}`);
-        }
-        else response = await authClient.get(`/portfolio/${id}`);
+        } else response = await authClient.get(`/portfolio/${id}`);
         setPortfolio(response.data.portfolio);
         console.log(response.data);
       } catch (err) {
@@ -41,6 +42,11 @@ const PortfolioPage = () => {
     };
     fetchData();
   }, []);
+
+  const onClickSendHandler = (displayName, id) => {
+    console.log(displayName, id);
+    navigate();
+  };
 
   let show;
   if (portfolio) {
@@ -51,17 +57,25 @@ const PortfolioPage = () => {
           {`ประเภทงานทั้งหมด > ${portfolio.category} >  ${portfolio.name}`}
         </Path>
         <Image src={PortfolioExample}></Image>
-        <FirstRow><Title>{portfolio.name}</Title> <Title>{portfolio.price} บาท</Title></FirstRow>
-        
+        <FirstRow>
+          <Title>{portfolio.name}</Title> <Title>{portfolio.price} บาท</Title>
+        </FirstRow>
         <Description>{portfolio.description}</Description>
-        <Duration><ImageDuration src={DurationIcon}/>{`${portfolio.duration} Day`}</Duration>
+        <Duration>
+          <ImageDuration src={DurationIcon} />
+          {`${portfolio.duration} Day`}
+        </Duration>
         <ProfileCard
+          onClick={onClickSendHandler.bind(
+            null,
+            portfolio.freelance.display_name,
+            portfolio.freelance.id
+          )}
           userImage={UserImage}
           username={portfolio.freelance.display_name}
           portCount={11}
           avgTime={5}
         />
-        
       </>
     );
   } else {
