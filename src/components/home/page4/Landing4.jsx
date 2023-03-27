ae9f7c136401d45e5108a1bf6eacf430214cd17f
patcharapongk <6230362521@student.chula.ac.th>
2023-03-27 18:37:52 +0700

    Added ReviewComment card and its assets

--- src/components/home/page4/Landing4.jsx
+++ src/components/home/page4/Landing4.jsx
@@ -1,8 +1,31 @@
+import tw from "twin.macro"
+import Card from "../../../assets/NewHomePage/page4/Card";
+
+import { ReviewData } from "./ReviewData";
+
+const Container = tw.div`
+    snap-center
+    flex flex-col items-center justify-center
+    w-full h-screen
+`
+
 const Landing4 = () => {
     return ( 
-        <div>
-            
-        </div>
+        <Container>
+            {
+                ReviewData.map((data, index) => {
+                    return (
+                        <Card 
+                            img={data.img}
+                            title={data.title}
+                            subtitle={data.subtitle}
+                            desc={data.desc}
+                            key={data.title}
+                        />
+                    )
+                })
+            }
+        </Container>
      );
 }
  
---
+++ src/components/home/page4/ReviewData.js
@@ -0,0 +1,18 @@
+
+import Avatar1 from "../../../assets/NewHomePage/page4/Avatar1.png"
+import Avatar2 from "../../../assets/NewHomePage/page4/Avatar2.png"
+
+export const ReviewData = [
+    {
+        img: Avatar1,
+        title: "อริสรา สายชม",
+        subtitle: "CEO of BookBix",
+        desc: "ใช้ CU Freelance ช่วยประหยัดเวลาได้มาก หางานง่าย"
+    },
+    {
+        img: Avatar2,
+        title: "อริสรา สายชม",
+        subtitle: "CEO of BookBix",
+        desc: "ใช้ CU Freelance ช่วยประหยัดเวลาได้มาก หางานง่าย"
+    }
+]
