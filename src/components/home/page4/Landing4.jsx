a872803916a44318a9b4cc5dbd1cb3126cc58425
patcharapongk <6230362521@student.chula.ac.th>
2023-03-27 21:28:55 +0700

    Added landing page 4 v.2 with animation and carousel

--- src/components/home/page4/Landing4.jsx
+++ src/components/home/page4/Landing4.jsx
@@ -2,7 +2,7 @@ import tw from "twin.macro"
 
 import React from 'react';
 import { motion } from "framer-motion";
-import { useSnapCarousel } from 'react-snap-carousel';
+import { SnapList, SnapItem } from 'react-snaplist-carousel';
 
 import { fadeIn } from "../../../assets/NewHomePage/fadeIn";
 
@@ -17,64 +17,28 @@ const Container = tw.div`
      h-screen overflow-hidden
 `
 
-const Item = tw(motion.div)`min-h-[20rem] min-w-[30rem] p-10`
-const Img = tw.img`w-full h-full rounded-[2rem]`
 
-const Elem = () => {
-    const { scrollRef, pages, activePageIndex, next, prev, goTo } =
-        useSnapCarousel();
-    return (
-        <>
-           
-            <div tw=" ">
-            <ul
-                ref={scrollRef}
-                tw="relative flex flex-row overflow-hidden snap-x snap-mandatory"
-            >
-                {
-                    ReviewData.map((data, index) => {
-                    return (
-                        <motion.div
-                            tw="min-h-[10rem] min-w-[20rem] py-4 px-2"
-                            transition={{ duration: 3 }}
-                        >
-                        <Card 
-                             img={data.img}
-                             title={data.title}
-                             subtitle={data.subtitle}
-                             desc={data.desc}
-                             key={data.id}
-                         />
-                        </motion.div>
-                    );
-                })}
-            </ul>
-                  
-            </div>
-            <div tw="relative w-screen mx-auto"> 
-                     <button tw="absolute bottom-[8rem] left-0" onClick={() => prev()}>Prev</button>
-                    <button tw="absolute bottom-[8rem] right-0"onClick={() => next()}>Next</button>
-            </div>
-            <div tw=" flex flex-col gap-2 items-center">
-                {activePageIndex + 1} / {pages.length}
-
-            </div>
-            {/* <ol style={{ display: 'flex' }}>
-                {pages.map((_, i) => (
-                    <li key={i}>
-                        <button
-                            style={i === activePageIndex ? { opacity: 0.5 } : {}}
-                            onClick={() => goTo(i)}
-                        >
-                            {i + 1}
-                        </button>
-                    </li>
-                ))}
-            </ol> */}
-        </>
-    );
-};
-
+const Elem = () => (
+    <SnapList>
+    {
+        ReviewData.map((data, index) => {
+          return (
+              <SnapItem margin={{ left: '15px', right: '15px' }} snapAlign="center">
+                  <Card 
+                                img={data.img}
+                                title={data.title}
+                                subtitle={data.subtitle}
+                                desc={data.desc}
+                                key={data.id}
+                            />
+                  </SnapItem>
+          )
+      })
+    }
+     
+      
+    </SnapList>
+  );
 
 const Cards = () => {
     return (
@@ -123,9 +87,9 @@ const Landing4 = () => {
                 initial="hidden"
                 whileInView={"show"}
                 // viewport={{ once: false, amount: 1 }}
-                tw=""
+                tw="w-screen"
                 >
-                <Cards/>
+                <Elem/>
           </motion.div>
           <QuoteImg src={QuoteR} tw="mr-8 dt:mr-16 self-end"/>
         </Container>
