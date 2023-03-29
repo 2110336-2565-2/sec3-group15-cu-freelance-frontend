9c61a7f8c9cd5ffbf202c673051dbc888d484d91
Theerut <theerut.seek@gmail.com>
2023-03-29 20:12:16 +0700

    Updated Login border

--- src/components/share/AuthTypeCard.jsx
+++ src/components/share/AuthTypeCard.jsx
@@ -1,10 +1,10 @@
 import tw, {styled} from "twin.macro"
 const Container = styled.div(()=>[
-    tw`flex box-border rounded-[20px] border-4 border-black w-full ip8:w-[250px] dt:max-w-full dt:w-2/5 flex-row 
-    dt:flex-col shadow-inherit dt:relative dt:h-[450px] items-center justify-between px-4`
+    tw`flex box-border rounded-[20px] border-2 border-black w-full ip8:w-[250px] dt:max-w-full dt:w-2/5 flex-row 
+    dt:flex-col shadow-inherit dt:relative dt:h-[450px] items-center justify-between px-4 hover:border-[#D62B70] pf:w-1/4`
 ]);
 const Type = styled.div(()=>[
-    tw`font-ibm font-bold text-mobile-h2 dt:text-desktop-h2 dt:text-center my-4`
+    tw`font-ibm font-bold text-mobile-h2 dt:text-desktop-h2 dt:text-center my-4 dt:text-2xl`
 ])
 const Avatar = styled.img(()=>[
     tw`dt:absolute dt:left-[50%] dt:translate-x-[-50%] dt:bottom-[-10%] h-[50px] dt:h-full`
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Title = styled.div(()=>[
   tw`text-center font-ibm font-bold text-mobile-h1 dt:text-3xl m-14 dt:m-14 pf:m-24 ip8:mb-24`
 ])
 const AuthChoiceContainer = styled.div(()=>[
-  tw`flex flex-col dt:flex-row px-8 dt:px-16 w-full items-center dt:justify-between gap-y-10`
+  tw`flex flex-col dt:flex-row px-8 dt:px-16 w-full items-center dt:justify-center gap-y-10 gap-x-16`
 ])
 const Login = () => {
   const navigate = useNavigate();
