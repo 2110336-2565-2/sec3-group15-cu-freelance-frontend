e6af425947d1db546443edbc857acf339c8bdecd
nartgg35665 <77729065+nartgg35665@users.noreply.github.com>
2023-04-10 10:42:54 +0700

    Updated sth

--- src/components/share/Backdrop.jsx
+++ src/components/share/Backdrop.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import tw from "twin.macro";
 
-const Backdrop = props => {
+const Backdrop = (props) => {
   return ReactDOM.createPortal(
-    <div tw="fixed z-[70] w-screen h-screen bg-black/50 top-0 left-0" onClick={props.onClick}></div>,
-    document.getElementById('backdrop-hook')
+    <div
+      tw="fixed z-[70] w-screen h-screen bg-black/50 top-0 left-0"
+      onClick={props.onClick}
+    ></div>,
+    document.getElementById("backdrop-hook")
   );
 };
 
